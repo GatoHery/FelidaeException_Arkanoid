ed8feabe2e8eb5975ff501c25e738b03fb75c70e
Natalia Solorzano <00120319@uca.edu.sv>
2020-06-06 17:19:12 -0600

    Add: CustomPictureBox
    Add: CustomPictureBox

---
+++ Plan B/CustomPictureBox.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace Plan_B
+{
+    public class CustomPictureBox : PictureBox
+    {
+        public int Golpes { get; set; }
+
+        public CustomPictureBox() : base() { }
+    }
+}
