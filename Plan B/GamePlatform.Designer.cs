15ceb632f6d8b2085e39608793d69f8918f223b2
Natalia Solorzano <00120319@uca.edu.sv>
2020-06-06 17:18:56 -0600

    Add: GamePlatform
    Add: GamePlatform

---
+++ Plan B/GamePlatform.Designer.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+
+namespace Plan_B
+{
+    partial class GamePlatform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picPaddle = new System.Windows.Forms.PictureBox();
+            this.GamePlatformTimer_Tick = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize) (this.picPaddle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPaddle
+            // 
+            this.picPaddle.Location = new System.Drawing.Point(515, 934);
+            this.picPaddle.Name = "picPaddle";
+            this.picPaddle.Size = new System.Drawing.Size(240, 86);
+            this.picPaddle.TabIndex = 0;
+            this.picPaddle.TabStop = false;
+            // 
+            // GamePlatformTimer_Tick
+            // 
+            this.GamePlatformTimer_Tick.Tick += new System.EventHandler(this.GamePlatformTimer_Tick_Tick);
+            // 
+            // GamePlatform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1480, 1075);
+            this.Controls.Add(this.picPaddle);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "GamePlatform";
+            this.Text = "GamePlatform";
+            this.Load += new System.EventHandler(this.GamePlatform_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GamePlatform_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GamePlatform_MouseMove);
+            ((System.ComponentModel.ISupportInitialize) (this.picPaddle)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPaddle;
+        private System.Windows.Forms.Timer GamePlatformTimer_Tick;
+    }
+}
---
+++ Plan B/GamePlatform.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Plan_B
+{
+    public partial class GamePlatform : Form
+    {
+        private CustomPictureBox[,] cpb;
+        private PictureBox ball;
+        
+        public GamePlatform()
+        {
+            InitializeComponent();
+            Height = ClientSize.Height;
+            Width = ClientSize.Width;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void GamePlatform_Load(object sender, EventArgs e)
+        {
+            picPaddle.BackgroundImage = Image.FromFile("../../Textures/Player.png");
+            picPaddle.BackgroundImageLayout = ImageLayout.Stretch;
+
+            picPaddle.Top = Height - picPaddle.Height - 80;
+            picPaddle.Left = (Width / 2) - (picPaddle.Width / 2);
+            
+            ball = new PictureBox();
+            ball.Width = ball.Height = 20;
+            ball.BackgroundImage = Image.FromFile("../../Textures/Ball.png");
+            ball.BackgroundImageLayout = ImageLayout.Stretch;
+
+            ball.Top = picPaddle.Top - ball.Height;
+            ball.Left = picPaddle.Left + (picPaddle.Width / 2) - (ball.Width / 2);
+            
+            Controls.Add(ball);
+
+            LoadTiles();
+            GamePlatformTimer_Tick.Start();
+
+        }
+
+
+        private void LoadTiles()
+        {
+            int xAxis = 10;
+            int yAxis = 5;
+
+            int PlatFormHeight = (int) (Height * 0.3) / yAxis;
+            int PlatFormWidth = (Width - (xAxis - 5)) / xAxis;
+            
+            cpb = new CustomPictureBox[yAxis, xAxis];
+
+            for (int i = 0; i < yAxis; i++)
+            {
+                for (int j = 0; j < xAxis; i++)
+                {
+                    cpb[i, j] = new CustomPictureBox();
+
+                    if (i == 0)
+                    {
+                        cpb[i, j].Golpes = 2;
+                    }
+                    else
+                    {
+                        cpb[i, j].Golpes = 1;
+                    }
+
+                    //Position from Height, Position from Width
+                    cpb[i, j].Height = PlatFormHeight;
+                    cpb[i, j].Width = PlatFormWidth;
+                    
+                    
+                    //Position from Left, Position from Top
+
+                    cpb[i, j].Left = j * PlatFormWidth;
+                    cpb[i, j].Top = i * PlatFormHeight;
+
+                    //If the value from i = 0, then put on the route from the brick pictureBox
+                    cpb[i, j].BackgroundImage = Image.FromFile("../../Textures" + GRN() + ".png");
+                    cpb[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+
+                    cpb[i, j].Tag = "tileTag";
+                    
+                    Controls.Add(cpb[i,j]);
+
+                }
+            }
+        }
+
+        private int GRN()
+        {
+            return new Random().Next(1, 8);
+        }
+        
+        private void GamePlatform_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!DatosJuego.juegoIniciado)
+            {
+                if (e.X < (Width - picPaddle.Width))
+                {
+                    picPaddle.Left = e.X;
+                    ball.Left = picPaddle.Left + (picPaddle.Width / 2) - (ball.Width / 2);
+                }
+            }
+            else
+            {
+                if (e.X < (Width - picPaddle.Width))
+                    picPaddle.Left = e.X;
+            }
+            
+        }
+
+        private void GamePlatformTimer_Tick_Tick(object sender, EventArgs e)
+        {
+            if (!DatosJuego.juegoIniciado)
+            {
+                return;
+            }
+
+            ball.Left += DatosJuego.dirX;
+            ball.Top += DatosJuego.dirY;
+
+            rebotarPelota();
+        }
+        
+        
+        private void GamePlatform_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                DatosJuego.juegoIniciado = true;
+
+        }
+
+        private void rebotarPelota()
+        {
+            if(ball.Bottom > Height)
+                Application.Exit();
+
+            if (ball.Left < 0 || ball.Right > Width)
+            {
+                DatosJuego.dirX = -DatosJuego.dirX;
+                return;
+            }
+
+            if (ball.Bounds.IntersectsWith(picPaddle.Bounds))
+            {
+                DatosJuego.dirY = -DatosJuego.dirY;
+            }
+
+            for (int i = 4; i >= 0; i--)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+                    if (ball.Bounds.IntersectsWith(cpb[i,j].Bounds))
+                    {
+                        cpb[i, j].Golpes--;
+
+                        if (cpb[i,j].Golpes == 0)
+                        {
+                            Controls.Remove(cpb[i,j]);
+                        }
+
+                        DatosJuego.dirY = -DatosJuego.dirY;
+                        
+                        return;
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
