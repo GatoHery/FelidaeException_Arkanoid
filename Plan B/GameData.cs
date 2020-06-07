8ddf8f9913296f7c922e236ece10e36ad49887c7
Natalia Solorzano <00120319@uca.edu.sv>
2020-06-07 14:02:37 -0600

    Add: GameData
    Add: GameData

---
+++ Plan B/GameData.cs
@@ -0,0 +1,9 @@
+﻿namespace Plan_B
+{
+    public class GameData
+    {
+        //This function allow to modify the direction from the ball
+        public static bool GameStarted = false;
+        public static int dirX = 10, dirY = -dirX;
+    }
+}
